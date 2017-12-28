a42fc96ac828919a835a176d38bad1cbc25dbaed
Antonio Lima <limaa91@gmail.com>
2017-12-28 20:02:31 +0000

    redefined targetPositions array for King.

--- app/src/main/java/co/antoniolima/onlinechess/King.java
+++ app/src/main/java/co/antoniolima/onlinechess/King.java
@@ -1,5 +1,6 @@
 package co.antoniolima.onlinechess;
 
+import static co.antoniolima.onlinechess.Constants.BOARD_WIDTH;
 import static co.antoniolima.onlinechess.Constants.DRAWABLE_BLACK_PIECE_KING;
 import static co.antoniolima.onlinechess.Constants.DRAWABLE_WHITE_PIECE_KING;
 
@@ -12,16 +13,35 @@ public class King extends Piece {
         this.setIdWhiteImage(DRAWABLE_WHITE_PIECE_KING);
         this.setIdBlackImage(DRAWABLE_BLACK_PIECE_KING);
         this.hasMadeFirstMove = false;
-        int [] targetPositions = new int [8];
-        targetPositions[0] = -9;
-        targetPositions[1] = -8;
-        targetPositions[2] = -7;
-        targetPositions[3] = -1;
-        targetPositions[4] = 1;
-        targetPositions[5] = 7;
-        targetPositions[6] = 8;
-        targetPositions[7] = 9;
-        this.setTargetPositionArray(targetPositions);
+        this.initTargetPositions();
+    }
+
+    @Override
+    public void initTargetPositions(){
+        this.resetTargetPositions();
+        int pieceX = this.getPosition()%BOARD_WIDTH;
+        int pieceY = this.getPosition()/BOARD_WIDTH;
+        this.addTargetPosition(new Position(pieceX, pieceY-1));         // norte
+        this.addTargetPosition(new Position(pieceX+1, pieceY-1));   // nordeste
+        this.addTargetPosition(new Position(pieceX+1, pieceY));         // este
+        this.addTargetPosition(new Position(pieceX+1, pieceY+1));   // sudeste
+        this.addTargetPosition(new Position(pieceX, pieceY+1));         // sul
+        this.addTargetPosition(new Position(pieceX-1, pieceY+1));   // sudoeste
+        this.addTargetPosition(new Position(pieceX-1, pieceY));        // oeste
+        this.addTargetPosition(new Position(pieceX-1, pieceY-1));   // nordoeste
+    }
+
+    @Override
+    public  void calculateTargetPositions(GameController gameController){
+
+        this.resetAvailablePositions();
+        this.initTargetPositions();
+        // cycle through targets array and check which positions are valid (within the board)
+        for (Position p : this.getTargetPositionsArray()) {
+            if (gameController.isThisPositionValid(p)){
+                p.setValid(true);
+            }
+        }
     }
 
     @Override
@@ -34,16 +54,18 @@ public class King extends Piece {
     @Override
     public void select(GameController gameController) {
 
-        this.findAvailablePositions(gameController);
+        this.calculateTargetPositions(gameController);
         gameController.resetHighlights();
-        // select itself
+        // highlight itself
         gameController.highlightPosition(this.getPosition());
-        // cycle through targets array and check which positions are valid (within the board)
-        for (int i = 0; i < this.getTargetPositionsArray().length; i++) {
-            if(gameController.isThisPositionValid(this.getPosition()+this.getTargetPosition(i)))
-                gameController.highlightPosition(this.getPosition()+this.getTargetPosition(i));
+        // highlight valid positions
+        for (Position p : this.getTargetPositionsArray()) {
+            if (p.isValid()){
+                gameController.highlightPosition(gameController.getUniCoordinate(p));
+            }
         }
     }
+
     @Override
     public void move(GameController gameController, int p) {
         hasMadeFirstMove = true;
