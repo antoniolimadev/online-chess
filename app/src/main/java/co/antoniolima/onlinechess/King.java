979a3183dbe2d1b59252e48c5752ba01068c036b
Antonio Lima <limaa91@gmail.com>
2017-12-28 17:24:25 +0000

    implemented target positions and select method for King.

--- app/src/main/java/co/antoniolima/onlinechess/King.java
+++ app/src/main/java/co/antoniolima/onlinechess/King.java
@@ -12,8 +12,38 @@ public class King extends Piece {
         this.setIdWhiteImage(DRAWABLE_WHITE_PIECE_KING);
         this.setIdBlackImage(DRAWABLE_BLACK_PIECE_KING);
         this.hasMadeFirstMove = false;
+        int [] targetPositions = new int [8];
+        targetPositions[0] = -9;
+        targetPositions[1] = -8;
+        targetPositions[2] = -7;
+        targetPositions[3] = -1;
+        targetPositions[4] = 1;
+        targetPositions[5] = 7;
+        targetPositions[6] = 8;
+        targetPositions[7] = 9;
+        this.setTargetPositionArray(targetPositions);
     }
 
+    @Override
+    public void findAvailablePositions(GameController gameController){
+        this.resetAvailablePositions();
+
+        // TODO: check if there are pieces in the way
+    }
+
+    @Override
+    public void select(GameController gameController) {
+
+        this.findAvailablePositions(gameController);
+        gameController.resetHighlights();
+        // select itself
+        gameController.highlightPosition(this.getPosition());
+        // cycle through targets array and check which positions are valid (within the board)
+        for (int i = 0; i < this.getTargetPositionsArray().length; i++) {
+            if(gameController.isThisPositionValid(this.getPosition()+this.getTargetPosition(i)))
+                gameController.highlightPosition(this.getPosition()+this.getTargetPosition(i));
+        }
+    }
     @Override
     public void move(GameController gameController, int p) {
         hasMadeFirstMove = true;
