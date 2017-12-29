aed04537d79b3bca99de9161ee9f8622c73add90
Jorge Teixeira <jorgemrt1992@gmail.com>
2017-12-29 14:01:54 +0000

    Queen movement, incomplete

--- app/src/main/java/co/antoniolima/onlinechess/Queen.java
+++ app/src/main/java/co/antoniolima/onlinechess/Queen.java
@@ -1,5 +1,6 @@
 package co.antoniolima.onlinechess;
 
+import static co.antoniolima.onlinechess.Constants.BOARD_WIDTH;
 import static co.antoniolima.onlinechess.Constants.DRAWABLE_BLACK_PIECE_QUEEN;
 import static co.antoniolima.onlinechess.Constants.DRAWABLE_WHITE_PIECE_QUEEN;
 
@@ -9,11 +10,164 @@ public class Queen extends Piece {
         super(color, position);
         this.setIdWhiteImage(DRAWABLE_WHITE_PIECE_QUEEN);
         this.setIdBlackImage(DRAWABLE_BLACK_PIECE_QUEEN);
+        this.initTargetPositions();
+    }
+
+    @Override
+    public void initTargetPositions(){
+        this.resetTargetPositions();
+        int pieceX = this.getPosition()%BOARD_WIDTH;
+        int pieceY = this.getPosition()/BOARD_WIDTH;
+        int x; // aux x
+        int y; // aux y
+
+        /////////////////////////////////////////////////////// NORDESTE
+        // reset a posicao da peça
+        x = pieceX;
+        y = pieceY;
+        // inicializa a primeira posicao da digonal nordeste a verificar
+        x++;
+        y--;
+        while(x < BOARD_WIDTH && y >= 0){
+            this.addTargetPosition(new Position(x, y));
+            x++;
+            y--;
+        }
+        //////////////////////////////////////////////////////// SUDESTE
+        // reset a posicao da peça
+        x = pieceX;
+        y = pieceY;
+        // inicializa a primeira posicao da digonal sudeste a verificar
+        x++;
+        y++;
+        while(x < BOARD_WIDTH && y < BOARD_WIDTH){
+            this.addTargetPosition(new Position(x, y));
+            x++;
+            y++;
+        }
+        /////////////////////////////////////////////////////// SUDOESTE
+        // reset a posicao da peça
+        x = pieceX;
+        y = pieceY;
+        // inicializa a primeira posicao da digonal sudoeste a verificar
+        x--;
+        y++;
+        while(x >= 0 && y < BOARD_WIDTH){
+            this.addTargetPosition(new Position(x, y));
+            x--;
+            y++;
+        }
+        /////////////////////////////////////////////////////// NOROESTE
+        // reset a posicao da peça
+        x = pieceX;
+        y = pieceY;
+        // inicializa a primeira posicao da digonal sudoeste a verificar
+        x--;
+        y--;
+        while(x >= 0 && y >= 0){
+            this.addTargetPosition(new Position(x, y));
+            x--;
+            y--;
+        }
+        /////////////////////////////////////////////////////// NORTE
+        // reset a posicao da peça
+        x = pieceX;
+        y = pieceY;
+        // inicializa a primeira posicao norte a verificar
+        //x++;
+        y++;
+        while(y < BOARD_WIDTH && y >= 0){
+            this.addTargetPosition(new Position(x, y));
+            //x++;
+            y++;
+        }
+        /////////////////////////////////////////////////////// SUL
+        // reset a posicao da peça
+        x = pieceX;
+        y = pieceY;
+        // inicializa a primeira posicao norte a verificar
+        //x++;
+        y--;
+        while(y < BOARD_WIDTH && y >= 0){
+            this.addTargetPosition(new Position(x, y));
+            //x++;
+            y--;
+        }
+        /////////////////////////////////////////////////////// ESTE
+        // reset a posicao da peça
+        x = pieceX;
+        y = pieceY;
+        // inicializa a primeira posicao norte a verificar
+        x++;
+        //y--;
+        while(x < BOARD_WIDTH && x >= 0){
+            this.addTargetPosition(new Position(x, y));
+            x++;
+            //y--;
+        }
+        /////////////////////////////////////////////////////// OESTE
+        // reset a posicao da peça
+        x = pieceX;
+        y = pieceY;
+        // inicializa a primeira posicao norte a verificar
+        x--;
+        //y--;
+        while(x < BOARD_WIDTH && x >= 0){
+            this.addTargetPosition(new Position(x, y));
+            x--;
+            //y--;
+        }
+    }
+
+    @Override
+    public void calculateTargetPositions(GameController gameController){
+        this.resetAvailablePositions();
+        this.initTargetPositions();
+        // cycle through targets array and check which positions are within the board
+        for (Position p : this.getTargetPositionsArray()) {
+            if (gameController.isThisPositionValid(p)){
+                // se ta livre, é valida
+                if (!gameController.isThisPositionTaken(gameController.getUniCoordinate(p))){
+                    p.setValid(true);
+                    // se ta ocupada, vê a cor da peça
+                } else {
+                    // se é diferente, é valida
+                    if (gameController.getPieceByPosition(gameController.getUniCoordinate(p)).getColor() != this.getColor()){
+                        p.setValid(true);
+                    }
+                }
+            }
+        }
+    }
+
+    @Override
+    public void select(GameController gameController) {
+
+        this.calculateTargetPositions(gameController);
+        gameController.resetHighlights();
+        // highlight itself
+        gameController.highlightPosition(this.getPosition());
+        // highlight valid positions
+        for (Position p : this.getTargetPositionsArray()) {
+            if (p.isValid()){
+                gameController.highlightPosition(gameController.getUniCoordinate(p));
+            }
+        }
     }
 
     @Override
     public void move(GameController gameController, int p) {
-        gameController.resetHighlights();
-        this.setPosition(p);
+        // verifica se p é uma posicao das validas
+        for (Position pos : this.getTargetPositionsArray()) {
+            if (gameController.getUniCoordinate(pos) == p){
+                gameController.resetHighlights();
+                this.setPosition(p);
+                // se a peça é movida, deixa de estar selecionada
+                gameController.setSelectedPiece(null);
+
+            }
+        }
+        //limpa target positions
+        this.resetTargetPositions();
     }
 }
